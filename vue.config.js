7f995c744b96f6edd26c31e94b94f86e5a4c6f26
explore11 <prefect_start@163.com>
2022-08-12 10:11:16 +0800

    修改路径

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 let proxyObj={};
 proxyObj['/ws']={
   ws:true,
-  target:"ws://172.168.180.131:8082"
+  target:"ws://localhost:8082"
 };
 proxyObj['/']={
   ws:false,
-  target:'http://172.168.180.131:8082',
+  target:'http://localhost:8082',
   changeOrigin: true,
   pathRewrite:{
     '^/':''
